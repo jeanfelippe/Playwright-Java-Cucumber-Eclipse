8e9e90d1ba7e78f56540d6e868094049871932bd
jean felippe <jeanfelippe_20@hotmail.com>
2024-12-19 09:54:04 -0300

    atualizando report.json no runner

--- src/test/java/runner/RunCake.java
+++ src/test/java/runner/RunCake.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/resources/features/loginstaging.feature",glue={"steps"}, plugin= {"html:target/cucumber-reports/cucumber-html-report.html"
-		, "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+		,"json:target/report.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class RunCake extends AbstractTestNGCucumberTests {
 	
 
